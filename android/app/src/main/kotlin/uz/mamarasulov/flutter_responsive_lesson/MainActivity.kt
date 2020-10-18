88e770fd3894036136e66f16431e6cb5dc39b377
Zayniddin <dsasaMNs#d2>
2020-10-18 16:47:00 +0500

    first commit

---
+++ android/app/src/main/kotlin/uz/mamarasulov/flutter_responsive_lesson/MainActivity.kt
@@ -0,0 +1,6 @@
+package uz.mamarasulov.flutter_responsive_lesson
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations(
+  //     [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _showChart = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('App'),
+        ),
+        body: isPortrait
+            ? Container(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Switch(
+                          value: _showChart,
+                          onChanged: (val) {
+                            setState(() {
+                              _showChart = val;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: size.height * 0.5,
+                      color: Colors.amberAccent,
+                      margin: EdgeInsets.all(20),
+                    ),
+                    Container(
+                      height: size.height * 0.1,
+                      color: Colors.amberAccent,
+                      margin: EdgeInsets.all(20),
+                    ),
+                  ],
+                ),
+              )
+            : Container(
+                child: Row(
+                  children: [
+                    Container(
+                      width: 300,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      width: 300,
+                      color: Colors.purple,
+                    ),
+                  ],
+                ),
+              ));
+  }
+}
